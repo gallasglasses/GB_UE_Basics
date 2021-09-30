0d0b8467d4800cd956506af48215f0c2a9b74014
gallasglasses <gallasglasses@gmail.com>
2021-09-30 19:01:38 +0300

    initcommit HW1
    yeep, it's HW1!

---
+++ HW1/Source/HW1.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class HW1Target : TargetRules
+{
+	public HW1Target(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("HW1");
+	}
+}
