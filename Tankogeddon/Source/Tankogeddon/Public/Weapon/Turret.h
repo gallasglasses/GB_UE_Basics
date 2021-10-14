4615aa0a42c939887729ce09dbaa4eb652959fa2
gallasglasses <gallasglasses@gmail.com>
2021-10-14 16:29:09 +0300

    Add Turret

--- Tankogeddon/Source/Tankogeddon/Public/Weapon/Turret.h
+++ Tankogeddon/Source/Tankogeddon/Public/Weapon/Turret.h
@@ -1,25 +1,70 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Turret.generated.h"
+
+class UStaticMeshComponent;
+class UStaticMeshComponent;
+class UArrowComponent;
+class UBoxComponent;
+class ATCannon;
+class APawn;
 
 UCLASS()
 class TANKOGEDDON_API ATurret : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	ATurret();
 
 protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+		UStaticMeshComponent* BodyMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+		UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+		UArrowComponent* CannonSetupPoint;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+		UBoxComponent* HitCollider;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammo")
+		TSubclassOf<ATCannon> CannonClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Targeting")
+		float TargetingRange = 1000.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Targeting")
+		float TargetingSpeed = 0.1f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Targeting")
+		float Accuracy = 10.f;
+
+	const FString BodyMeshPath = "StaticMesh'/Game/CSC/Meshes/SM_CSC_Tower1.SM_CSC_Tower1'";
+	const FString TurretMeshPath = "StaticMesh'/Game/CSC/Meshes/SM_CSC_Gun1.SM_CSC_Gun1'";
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void Destroyed() override;
+	void Targeting();
+	void RotateToPlayer();
+	bool IsPlayerInRange();
+	bool CanFire();
+	void Fire();
 
-public:	
+private:
+	UPROPERTY()
+		ATCannon* Cannon;
+
+	UPROPERTY()
+		APawn* PlayerPawn;
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
